a7b179b370cf42137823d9813e8469c62d8c03cb
Allysson Santos <68858220+Allyssonantos@users.noreply.github.com>
2021-02-24 15:32:04 -0300

    NLW #4

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect, useContext } from 'react';
-import { ChallengesContext } from '../contexts/ChallengesContext';
+import { ChallengesContext } from '../contexts/ChallengeContext';
 import styles from '../styles/components/Countdown.module.css';
 
 let countdownTimeout: NodeJS.Timeout
@@ -84,3 +84,6 @@ export function Countdown() {
  
     </div>
   )
+}
+
+      
