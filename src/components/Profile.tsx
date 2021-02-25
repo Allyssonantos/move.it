b6b5692614d084672d2c8f1084faa9ad943fa87e
Allysson Santos <68858220+Allyssonantos@users.noreply.github.com>
2021-02-25 10:34:43 -0300

    Aura 04

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,6 +1,9 @@
+import { useContext } from 'react'
+import { ChallengesContext } from '../contexts/ChallengeContext'
 import styles from '../styles/components/Profile.module.css'
 
 export function Profile() {
+  const { level } = useContext(ChallengesContext)
   return (
     <div className={styles.profileContainer}>
       <img src="https://avatars.githubusercontent.com/u/68858220?s=460&u=ef8deedb95e9ad40fdb7f027213a2f933684c88d&v=4" alt="Allysson Santos"/>
@@ -8,7 +11,7 @@ export function Profile() {
         <strong>Allysson Santos</strong>
         <p>
           <img src="icons/level.svg" alt="Level"/>
-          Level 1
+          Level {level}
         </p>
       </div>
     </div>
